18a9076ddba4e578740ef5f4f1df3cda03c25007
SushantAmin17 <sushantamin81@gmail.com>
2024-03-01 12:33:16 +0530

    New File Added

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
-{/* <div id="parent">
-    <div id="child1">
-        <h1>Header1</h1>
-        <h2>Header2</h2>
-    </div>
-    <div id="child2">
-        <h1>Header1</h1>
-        <h2>Header2</h2>
-    </div>
-</div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 
-const para = React.createElement(
-    "div", {id: "parent"},
-    [
-        React.createElement( "div",{id: "child1"},
-            [
-                React.createElement("h1",{},"This is h1 tag"),
-                React.createElement("h2",{},"This is h2 tag")
-            ]
-        ),
-
-        React.createElement( "div",{id: "child2"},
-            [
-                React.createElement("h1",{},"This is h1 tag"),
-                React.createElement("h2",{},"This is h2 tag")
-            ]
-        ),
-    ]
-)
+const heading = <h1>This is JSX Code</h1>
 
 
 
@@ -41,4 +16,4 @@ const para = React.createElement(
 // console.log(heading); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(para);
+root.render(heading);
